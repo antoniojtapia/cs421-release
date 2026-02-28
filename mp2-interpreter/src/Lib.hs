490b552c52a60421ad4cbef57b54a69fe8019feb
Antonio Tapia <antoniojtapia251@gmail.com>
2026-02-27 23:58:43 -0600

    Finish MP2

--- mp2-interpreter/src/Lib.hs
+++ mp2-interpreter/src/Lib.hs
@@ -88,10 +88,12 @@ liftIntOp op (IntVal x) (IntVal y) = IntVal $ op x y
 liftIntOp _ _ _ = ExnVal "Cannot lift"
 
 liftBoolOp :: (Bool -> Bool -> Bool) -> Val -> Val -> Val
-liftBoolOp = undefined
+liftBoolOp op (BoolVal x) (BoolVal y) = BoolVal $ op x y
+liftBoolOp _ _ _ = ExnVal "Cannot lift"
 
 liftCompOp :: (Int -> Int -> Bool) -> Val -> Val -> Val
-liftCompOp = undefined
+liftCompOp op (IntVal x) (IntVal y) = BoolVal $ op x y
+liftCompOp _ _ _ = ExnVal "Cannot lift"
 
 --- Eval
 --- ----
@@ -100,36 +102,55 @@ eval :: Exp -> Env -> Val
 
 --- ### Constants
 
-eval (IntExp i)  _ = undefined
-eval (BoolExp i) _ = undefined
+eval (IntExp i)  _ = IntVal i
+eval (BoolExp i) _ = BoolVal i
 
 --- ### Variables
 
-eval (VarExp s) env = undefined
+eval (VarExp s) env
+    | Just val <- H.lookup s env = val
+    | otherwise = ExnVal "No match in env"
 
 --- ### Arithmetic
 
-eval (IntOpExp op e1 e2) env = undefined
+eval (IntOpExp "/" e1 e2) env -- handle division by 0 case separately
+    | IntVal 0 <- v2 = ExnVal "Division by 0"
+    | otherwise = liftIntOp div v1 v2
+        where (v1, v2) = (eval e1 env, eval e2 env)
+eval (IntOpExp op e1 e2) env = liftIntOp intOp (eval e1 env) (eval e2 env)
+    where Just intOp = H.lookup op intOps
 
 --- ### Boolean and Comparison Operators
 
-eval (BoolOpExp op e1 e2) env = undefined
+eval (BoolOpExp op e1 e2) env = liftBoolOp boolOp (eval e1 env) (eval e2 env)
+    where Just boolOp = H.lookup op boolOps
 
-eval (CompOpExp op e1 e2) env = undefined
+eval (CompOpExp op e1 e2) env = liftCompOp compOp (eval e1 env) (eval e2 env)
+    where Just compOp = H.lookup op compOps
 
 --- ### If Expressions
 
-eval (IfExp e1 e2 e3) env = undefined
+eval (IfExp e1 e2 e3) env
+    | BoolVal True <- eval e1 env = eval e2 env
+    | BoolVal False <- eval e1 env = eval e3 env
+    | otherwise = ExnVal "Condition is not a Bool"
 
 --- ### Functions and Function Application
 
-eval (FunExp params body) env = undefined
+eval (FunExp params body) env = CloVal params body env
 
-eval (AppExp e1 args) env = undefined
+eval (AppExp e1 args) env
+    | CloVal params body cloEnv <- eval e1 env =
+        let argVals = (`eval` env) <$> args
+            newEnv = H.union (H.fromList $ zip params argVals) cloEnv
+        in eval body newEnv
+    | otherwise = ExnVal "Apply to non-closure"
 
 --- ### Let Expressions
 
-eval (LetExp pairs body) env = undefined
+eval (LetExp pairs body) env =
+    let newEnv = H.union (H.fromList $ (\(s,e) -> (s, eval e env)) <$> pairs) env
+    in eval body newEnv
 
 --- Statements
 --- ----------
@@ -143,18 +164,33 @@ exec (PrintStmt e) penv env = (val, penv, env)
 
 --- ### Set Statements
 
-exec (SetStmt var e) penv env = undefined
+exec (SetStmt var e) penv env = ("", penv, H.insert var val env)
+    where val = eval e env
 
 --- ### Sequencing
 
-exec (SeqStmt []) penv env = undefined
+exec (SeqStmt []) penv env = ("", penv, env)
+exec (SeqStmt (s:ss)) penv env =
+    let (out1, penv2, env2) = exec s penv env
+        (out2, penv3, env3) = exec (SeqStmt ss) penv2 env2
+    in (out1 ++ out2, penv3, env3)
 
 --- ### If Statements
 
-exec (IfStmt e1 s1 s2) penv env = undefined
+exec (IfStmt e1 s1 s2) penv env
+    | BoolVal True <- eval e1 env = exec s1 penv env
+    | BoolVal False <- eval e1 env = exec s2 penv env
+    | otherwise = (show $ ExnVal "Condition is not a Bool", penv, env)
 
 --- ### Procedure and Call Statements
 
-exec p@(ProcedureStmt name args body) penv env = undefined
+exec p@(ProcedureStmt name args body) penv env = 
+    let newPenv = H.insert name p penv
+    in ("", newPenv, env)
 
-exec (CallStmt name args) penv env = undefined
+exec (CallStmt name args) penv env
+    | Just (ProcedureStmt _ params body) <- H.lookup name penv =
+        let argVals = (`eval` env) <$> args
+            newEnv = H.union (H.fromList $ zip params argVals) env
+        in exec body penv newEnv
+    | otherwise = ("Procedure not found: " ++ name, penv, env)
